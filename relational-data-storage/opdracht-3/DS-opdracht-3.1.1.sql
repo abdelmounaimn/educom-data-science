69a215c96de74110ca04d4ad7609f16d7a0afc03
abdelmounaimn <abdelmounaimne@gmail.com>
2022-11-08 12:23:11 +0100

    #3.1.2

--- relational-data-storage/opdracht-3/DS-opdracht-3.1.1.sql
+++ relational-data-storage/opdracht-3/DS-opdracht-3.1.1.sql
@@ -1,3 +1,10 @@
 use mhl;
 -- Selecteer de inhoud uit de stedentabel
 SELECT * FROM mhl_cities;
+
+-- select 3.1.2 Selecteer de unieke steden uit de stedentabel
+use mhl;
+SELECT DISTINCT  name  FROM mhl_cities;
+-- 3.1.3 Selecteer naam, straat, huisnummer en postcode van alle leveranciers
+SELECT name, straat, huisnr, postcode FROM mhl_suppliers;
+
