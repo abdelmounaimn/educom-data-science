347377f9f5fc0b4b2183af270fac8ac16535314c
abdelmounaimn <abdelmounaimne@gmail.com>
2022-11-08 16:30:44 +0100

    #3.2.4

--- relational-data-storage/opdracht-3/DS-opdracht-3.2.4.sql
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.4.sql
@@ -1,4 +1,4 @@
 use mhl;
 SELECT mhl_suppliers.name, mhl_suppliers.straat, mhl_suppliers.huisnr, mhl_suppliers.postcode 
 FROM mhl_suppliers join mhl_cities on mhl_cities.id =mhl_suppliers.city_ID
-WHERE mhl_cities.name  = "Amsterdam";
+WHERE mhl_cities.name  = "Amsterdam" or mhl_suppliers.p_city_ID in (select id from mhl_cities where name = 'Den Haag');
