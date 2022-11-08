72e1ab64b8846e7b43c8a8e43a36e6c8678ff8ad
abdelmounaimn <abdelmounaimne@gmail.com>
2022-11-08 16:38:14 +0100

    #3.2.6

--- relational-data-storage/opdracht-3/DS-opdracht-3.2.6.sql
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.6.sql
@@ -1,4 +1,4 @@
 use mhl;
 SELECT mhl_suppliers.name, mhl_suppliers.straat, mhl_suppliers.huisnr, mhl_suppliers.postcode 
-FROM mhl_suppliers join mhl_cities on mhl_cities.id =mhl_suppliers.city_ID
-WHERE mhl_cities.name  = "Amsterdam";
+FROM mhl_suppliers 
+WHERE mhl_suppliers.huisnr between 10 and 20 or mhl_suppliers.huisnr >= 100 ;
