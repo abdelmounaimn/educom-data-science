0b86da1fa6290e774be33469bbcbd14387e3dd10
abdelmounaimn <abdelmounaimne@gmail.com>
2022-11-09 13:49:06 +0100

    Op4 #4.1.5

---
+++ relational-data-storage/opdracht-4/DS-opdracht-4.1.5.sql
@@ -0,0 +1,7 @@
+SELECT sup.name, sup.straat, sup.huisnr, sup.postcode, pc.lat, pc.lng 
+FROM mhl_suppliers sup
+ join mhl_cities c on sup.city_ID = c.id
+ join mhl_communes com on com.id = c.commune_ID
+join mhl_districts d on d.id = com.district_ID
+join pc_lat_long pc on pc.id = d.display_order
+order by pc.lat desc limit 5
