3462cb88db134a2058bbf4479e73d0b61d923304
abdelmounaimn <abdelmounaimne@gmail.com>
2022-11-09 11:48:17 +0100

    Op4 #4.1.

---
+++ relational-data-storage/opdracht-4/DS-opdracht-4.1.1.sql
@@ -0,0 +1,3 @@
+ SELECT mhl_suppliers.name, mhl_suppliers.straat, mhl_suppliers.huisnr, mhl_suppliers.postcode 
+FROM mhl_suppliers join mhl_cities on mhl_cities.id =mhl_suppliers.city_ID
+WHERE mhl_cities.name  = "Amsterdam";
