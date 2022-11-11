5764702a48bdda701016d40adaf2931ee2a9cd4e
abdelmounaimn <abdelmounaimne@gmail.com>
2022-11-11 11:12:31 +0100

    #5.2.1

---
+++ relational-data-storage/opdracht-5/DS-opdracht-5.2.1.sql
@@ -0,0 +1,25 @@
+select s.name naam, 
+case 
+when con.name is null then 't.a.v. de directie'
+else con.name
+end as anhef,
+case 
+when s.p_address not like ''  then s.p_address 
+else concat(s.straat , ' ',  s.huisnr ) 
+end as adres ,
+case 
+when s.p_address not like ''  then s.p_address 
+else (select name from mhl_cities where mhl_cities.id=s.city_ID ) 
+end as stad ,
+case 
+when s.p_address not like ''  then s.p_postcode
+else s.postcode 
+end as postCode ,
+if(s.p_address like '',
+(select d.name as provincie from mhl_districts as d where id = (select district_ID from mhl_communes where id= (select commune_ID from mhl_cities where id=(s.city_ID)))),
+(select d.name as provincie from mhl_districts as d where id = (select district_ID from mhl_communes where id= (select commune_ID from mhl_cities where id=(s.p_city_ID))))
+) as provicie
+from mhl_suppliers as s
+left join mhl_contacts as con on s.id=con.supplier_ID and s.p_address not like '' 
+order by (provicie)
+
